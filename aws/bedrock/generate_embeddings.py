137a8836b6e91aa2bf7e84e53afc12b5d7fc1a6f
Sanchan Moses <sanchan.moses@mongodb.com>
2024-01-24 13:16:45 +0100

    feat: replace openai with bedrock

---
+++ aws/bedrock/generate_embeddings.py
@@ -0,0 +1,73 @@
+import os
+
+from langchain_community.document_loaders import CSVLoader, MongodbLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import MongoDBAtlasVectorSearch
+from pymongo import MongoClient
+
+import boto3
+
+session = boto3.Session(
+    aws_access_key_id=os.getenv("AWS_SERVER_PUBLIC_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SERVER_SECRET_KEY"),
+)
+
+# Setup bedrock
+bedrock_runtime = session.client(
+    service_name="bedrock-runtime",
+    region_name="us-east-1",
+)
+
+from langchain_community.llms import Bedrock
+
+model_id = os.environ["MODEL_ID"]
+model_kwargs =  { 
+    "maxTokenCount": 4096,
+    "temperature": 0.0,
+    "topP": 1,
+    "stopSequences": [],
+}
+llm = Bedrock(
+    client=bedrock_runtime,
+    model_id=model_id,
+    model_kwargs=model_kwargs
+)
+
+from langchain_community.embeddings import BedrockEmbeddings
+
+# create embeddings
+bedrock_embedding = BedrockEmbeddings(
+    client=bedrock_runtime,
+    model_id=os.environ["EMBEDDING_MODEL_ID"],
+)
+
+MONGODB_CONNECTION_STRING = os.environ["MONGODB_CONNECTION_STRING"]
+
+MONGODB_DB = os.environ["MONGODB_DB"]
+MONGODB_DB_COLLECTION = os.environ["MONGODB_DB_COLLECTION"]
+
+ATLAS_VECTOR_SEARCH_INDEX_NAME = os.environ["ATLAS_VECTOR_SEARCH_INDEX_NAME"]
+
+client = MongoClient(MONGODB_CONNECTION_STRING)
+db = client[MONGODB_DB]
+MONGODB_COLLECTION = db[MONGODB_DB_COLLECTION]
+
+loader = MongodbLoader(
+    connection_string=MONGODB_CONNECTION_STRING,    
+    db_name=MONGODB_DB,
+    collection_name=MONGODB_DB_COLLECTION,
+    filter_criteria={},
+)
+
+docs = loader.load()
+print(f"documents:loaded:size={len(docs)}")
+
+docs = CharacterTextSplitter(chunk_size=2000, chunk_overlap=400, separator=",").split_documents(docs)
+print(f"Documents:after split and chunking size={len(docs)}")
+
+_ = MongoDBAtlasVectorSearch.from_documents(
+    documents=docs,
+    embedding=bedrock_embedding,
+    collection=MONGODB_COLLECTION,
+    index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME,
+)
